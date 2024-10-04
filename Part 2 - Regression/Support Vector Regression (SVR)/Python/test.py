6e0589c5994056b984ff2d8fcf591ba513544207
VishnuRathore98 <rathorevishnu666@gmail.com>
2024-10-04 09:47:48 +0530

    Add: Support Vector Regression(SVR) -> initial

---
+++ Part 2 - Regression/Support Vector Regression (SVR)/Python/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+#z = np.reshape(y,(-1,1))
+y = y.reshape(len(y),1)
+
+# To calculate different mean and standard deviation for both X and y and use the
+# relevant one with each
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+
+X_standard = sc_X.fit_transform(X)
+y_standard = sc_y.fit_transform(y)
+
+print("X: ",X_standard)
+print("y: ",y_standard)
+
